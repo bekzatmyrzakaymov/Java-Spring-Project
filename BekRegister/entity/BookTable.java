e947463f1fce72ea2f633dbb21112027726722b4
bekzatmyrzakaymov <82397962+bekzatmyrzakaymov@users.noreply.github.com>
2021-10-18 22:15:53 +0600

    Add files via upload

---
+++ BekRegister/entity/BookTable.java
@@ -0,0 +1,34 @@
+package com.example.bek.BekRegister.entity;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="book")
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+public class BookTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String bookName;
+    private String bookAuthor;
+    private String genre;
+
+    public BookTable(String bookName, String bookAuthor, String genre) {
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.genre = genre;
+
+    }
+
+
+}
