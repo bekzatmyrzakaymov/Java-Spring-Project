ab390a135dcea38faf87ab908e0e944ebf017702
bekzatmyrzakaymov <82397962+bekzatmyrzakaymov@users.noreply.github.com>
2021-10-21 21:26:22 +0600

    Delete BekRegister directory

--- BekRegister/reposotory/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bek.BekRegister.reposotory;
-
-import com.example.bek.BekRegister.entity.UserTable;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserTable, Long> {
-    UserTable findByEmail(String email);
-}
